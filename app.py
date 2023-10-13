e8e6b1c36232891b0305f13d244d7a6e9ae9b1f2
evgeniyBay <75795640+evgeniyBay@users.noreply.github.com>
2023-10-13 14:49:41 +0500

    try this

--- app.py
+++ app.py
@@ -1,30 +1,48 @@
 from flask import Flask, render_template, request, make_response
+import json
+import os
 
 app = Flask(__name__)
 
-# Инициализация счетчиков
-visit_data = {
-    'total_visits': 0,
-    'unique_visits': set()
-}
+if os.path.exists('visit_data.json'):
+    with open('visit_data.json', 'r') as json_file:
+        data = json.load(json_file)
+        visit_data = {
+            'total_visits': data['total_visits'],
+            'unique_visits': set(data['unique_visits'])
+        }
+else:
+    visit_data = {
+        'total_visits': 0,
+        'unique_visits': set()
+    }
+
+def save_visit_data():
+    data = {
+        'total_visits': visit_data['total_visits'],
+        'unique_visits': list(visit_data['unique_visits'])
+    }
+    with open('visit_data.json', 'w') as json_file:
+        json.dump(data, json_file)
 
 
 @app.route('/')
 def home():
     global visit_data
     visit_data['total_visits'] += 1
-    session_id = request.cookies.get('session_id')
+    client_ip = request.remote_addr
 
-    if session_id is not None:
-        if session_id not in visit_data['unique_visits']:
-            visit_data['unique_visits'].add(session_id)
+    if client_ip not in visit_data['unique_visits']:
+        visit_data['unique_visits'].add(client_ip)
 
     response = make_response(render_template('index.html',
                                              visit_data=visit_data))
-    if session_id is None:
-        session_id = str(hash(request.user_agent))
-        response.set_cookie('session_id', session_id)
+
+    save_visit_data()
+
     return response
+
+
 
 
 if __name__ == '__main__':
